3cfdc1b393b76ea55d778a2d45a16b97be7466f6
Ayman Izzeldin <ayman.izzeldin@stfc.ac.uk>
2023-07-07 11:11:56 +0100

    initial commit

---
+++ ckanext/datasci_sharing/plugin.py
@@ -0,0 +1,18 @@
+import logging
+
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+logger = logging.getLogger(__name__)
+
+
+class DatasciSharingPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'datasci_sharing')
