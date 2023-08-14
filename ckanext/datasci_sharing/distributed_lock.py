af7f03146599622048a6a051e2d0d6dfe913b434
Ayman Izzeldin <ayman.izzeldin@stfc.ac.uk>
2023-08-14 03:55:30 +0100

    timeout distributed lock

--- ckanext/datasci_sharing/distributed_lock.py
+++ ckanext/datasci_sharing/distributed_lock.py
@@ -2,21 +2,30 @@ from contextlib import contextmanager
 import logging
 
 from ckan.lib.redis import connect_to_redis, is_redis_available
+from redis.exceptions import LockError as RedisLockError, LockNotOwnedError as RedisLockNotOwnedError
 
 
 logger = logging.getLogger(__name__)
 
 
-@contextmanager
-def distributed_lock(name: str):
-    if not is_redis_available():
-        logger.error("redis needs to be available to acquire a distributed lock")
-        yield
-        return
+class LockError(Exception): pass
 
-    with connect_to_redis().lock(f"datasci-sharing:lock-{name}") as lock:
-        logger.info(f"acquired lock {lock.name}")
-        try:
-            yield lock
-        finally:
-            logger.info(f"releasing lock {lock.name}")
+
+@contextmanager
+def distributed_lock(name: str, blocking_timeout=1, timeout=2):
+    if not is_redis_available():
+        raise LockError("redis is required to acquire a distributed lock")
+
+    try:
+        lock_name = f"datasci-sharing:lock-{name}"
+        logger.debug("acquiring lock %s", lock_name)
+        with connect_to_redis().lock(lock_name, blocking_timeout=blocking_timeout, timeout=timeout) as lock:
+            try:
+                logger.debug("acquired lock %s", lock_name)
+                yield lock
+            finally:
+                logger.debug("releasing lock %s", lock_name)
+    except (RedisLockError, RedisLockNotOwnedError) as e:
+        raise LockError(f"unable to acquire lock") from e
+    finally:
+        logger.debug("released lock %s", lock_name)
